d94c194aeca7cc2d177fc1c77d4bf07af57d1488
Kurt Frey <kfrey@idmod.org>
2025-09-07 19:54:46 -0700

    Typo

--- emod_api/demographics/DemographicsTemplates.py
+++ emod_api/demographics/DemographicsTemplates.py
@@ -639,7 +639,7 @@ def _EquilibriumAgeDistFromBirthAndMortRates(birth_rate=YearlyRate(40/1000.), mo
 def birthrate_multiplier(pop_dat_file: Path,
                          base_year: int,
                          start_year: int,
-                         max_daily_mort: float = 0.01) -> tuple(np.Array, np.Array):
+                         max_daily_mort: float = 0.01) -> tuple(np.ndarray, np.ndarray):
     """
     Create a birth rate multiplier from UN World Population data file.
     Args:
