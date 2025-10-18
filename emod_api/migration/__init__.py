51a7151d2accf7d3e9b5d518eea618ab01aff0e5
Kurt Frey <kfrey@idmod.org>
2025-10-18 10:55:45 -0700

    Remove gravity migration webservice

--- emod_api/migration/__init__.py
+++ emod_api/migration/__init__.py
@@ -1,2 +1,1 @@
-from .migration import Migration, from_file, from_params, from_demog_and_param_gravity, \
-    from_demog_and_param_gravity_webservice, examine_file, to_csv, from_csv
+from .migration import Migration, from_file, from_params, from_demog_and_param_gravity, examine_file, to_csv, from_csv
