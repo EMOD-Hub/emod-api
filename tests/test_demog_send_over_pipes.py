8707724dad658057bb4546621d7f75aa4fee5562
Kurt Frey <kfrey@idmod.org>
2025-10-23 13:26:15 -0700

    Reduce size of large demog file to save time

--- tests/test_demog_send_over_pipes.py
+++ tests/test_demog_send_over_pipes.py
@@ -1,10 +1,10 @@
-import os, sys
+import os
+import sys
 import json
 import unittest
 import emod_api.demographics.Demographics as Demog
 import emod_api.demographics.Node as Node
 import emod_api.demographics.DemographicsTemplates as DT
-# import manifest 
 from datetime import date
 import pandas as pd
 import numpy as np
@@ -17,8 +17,6 @@ import platform
 thisplatform = sys.platform
 
 class DemographicsTestMisc(unittest.TestCase):
-    short_expected = ""
-    short_demog=""
     large_demog =""
     large_expected=""
 
@@ -29,39 +27,27 @@ class DemographicsTestMisc(unittest.TestCase):
     def create_large_demog(cls):
         print ('called once before any tests in class')
         # Large demographics object.
-        print( "Creating big complex demographics." )
-        cls.large_demog =Demog.from_params(tot_pop=10000000, num_nodes=10000)
-        age_distribution = Distributions.AgeDistribution_SEAsia 
-        mort_distribution = Distributions.SEAsia_Diag
-        for node_id in range(10000):
-            cls.large_demog.SetAgeDistribution(age_distribution, [node_id+1])
-            cls.large_demog.SetMortalityDistribution(mort_distribution, [node_id+1])
+        num_node = 1000
+        print("Creating big complex demographics.")
+        cls.large_demog =Demog.from_params(tot_pop=10000000, num_nodes=num_node)
+
+        for node_id in range(num_node):
             cls.large_demog.SetFertilityOverTimeFromParams(years_region1=100,years_region2=10,start_rate=0.3,inflection_rate=0.25,end_rate=0.1, node_ids=[node_id+1])
-        
+            cls.large_demog.nodes[node_id].individual_attributes.age_distribution = Distributions.AgeDistribution_SEAsia
+            cls.large_demog.nodes[node_id].individual_attributes.mortality_distribution = Distributions.SEAsia_Diag
+
         # Large Expected Results Dictionary
         cls.large_expected = cls.large_demog.to_dict()
         print(f"Total of nodes: {cls.large_expected['Metadata']['NodeCount']} ... file size is aprox 54 MB")
-              
+
     def setUp(self) -> None:
         print(f"\n{self._testMethodName} started...")
         current_directory = os.path.dirname(os.path.realpath(__file__))
         demo_folder = os.path.join(current_directory, 'data', 'demographics')
         self.out_folder = demo_folder
 
-        # Creating Short file demog file.  
-        self.create_short_demog()
-
     def tearDown(self) -> None:
         print(f"\n{self._testMethodName} ...DONE...")
-
-    def create_short_demog(self):
-        # Creating short demog file.  
-        print(f"Creating demographic expected object for {self._testMethodName}")
-        short_expected_demog = ""
-        short_expected_demog =Demog.from_params(tot_pop=10000000, num_nodes=444)
-        
-        # short Expected dictionary
-        self.short_expected = short_expected_demog.to_dict()
 
     @unittest.skipIf(thisplatform.startswith("win"), "Not valid on Windows")
     def test_linux_send_over_file(self):
@@ -71,7 +57,6 @@ class DemographicsTestMisc(unittest.TestCase):
         tmpfile = tempfile.NamedTemporaryFile().name
         print( "Transfer data from another module using files." )
 
-        #mydemog = copy.deepcopy()   #<<<==== verify if this is a problem, ideally I would use a deepcopy.
         # Gets a copy of a large 
         mydemog = copy.copy(self.large_demog)
 
@@ -88,7 +73,7 @@ class DemographicsTestMisc(unittest.TestCase):
         actual = json.loads(read_data)
 
         print("Validating now Expected versus Actual demog objects")
-        
+
         # VALIDATION:
         self.assertDictEqual(expected, actual, "Demographic dict objects are different")
         print("...Validation Done...")
@@ -98,7 +83,6 @@ class DemographicsTestMisc(unittest.TestCase):
     def test_linux_send_over_pipe_basic(self):
         # FUNCTIONAL TEST CASE: Call to Send method using a valid Pipe Number
         # EXPECTED: Valid pipe number is handled and demographics file is processed.
-
 
         print( "The child will write text to a pipe and ")
         print( "the parent will read the text written by child...")
@@ -133,7 +117,6 @@ class DemographicsTestMisc(unittest.TestCase):
             # print(expected)
             #self.maxDiff = None
             self.assertDictEqual(expected, actual, "\n Demog.send() - using pipes - FAILED - Demographic dict objects are different")
-            
         elif processid == 0:
             print("Child process id:", processid)  # Should be zero
             os.close(r)     # Closes the parent connection
@@ -145,12 +128,12 @@ class DemographicsTestMisc(unittest.TestCase):
             w.close()
             print ("Child closing")
             #sys.exit()
-            
+
         if processid>0:
             print("Parent (main) method - Done..")
         elif processid==0:
             print("Child method - Done..")
-            
+
     @unittest.skipIf(thisplatform.startswith("win"), "Not valid on Windows")  
     def test_linux_send_over_named_pipe(self):
         # FUNCTIONAL TEST CASE: Call to Send method using a valid Named Pipe
@@ -173,14 +156,11 @@ class DemographicsTestMisc(unittest.TestCase):
             # reader
             fifo_reader = open( tmpfile, "r" )
             contents = fifo_reader.read()
-            #print ( contents   )
 
             sys.stdout.flush()
             actual = json.loads(contents)
-            # print(actual)
 
             expected = self.large_expected
-            # print(expected)
 
             # VALIDATION:
             self.assertDictEqual(expected, actual, "\n Demog.send() - using Named Pipes (os.mkfifo) - FAILED - Demographic dict objects are different")
@@ -197,7 +177,4 @@ class DemographicsTestMisc(unittest.TestCase):
             sys.stdout.flush()
             print ("Child (sender / writter) removing")
             os.remove( tmpfile )
-            #sys.exit()
- 
-if __name__ == '__main__':
-    unittest.main()
+
