8cdcd3a08f92bb2f62a7622dfea62fd942a23e84
Kurt Frey <kfrey@idmod.org>
2026-03-30 10:43:48 -0700

    Do not write empty objects in overlay node

--- emod_api/demographics/overlay_node.py
+++ emod_api/demographics/overlay_node.py
@@ -14,3 +14,28 @@ class OverlayNode(Node):
                  **kwargs
                  ):
         super().__init__(lat=latitude, lon=longitude, pop=initial_population, forced_id=node_id, **kwargs)
+
+    def to_dict(self) -> dict:
+        """
+        Translate node structure to a dictionary for EMOD
+        """
+        d = {"NodeID": self.id}
+
+        if self.node_attributes:
+            na_dict = self.node_attributes.to_dict()
+            if na_dict:
+                d["NodeAttributes"] = na_dict
+
+        if self.individual_attributes:
+            ia_dict = self.individual_attributes.to_dict()
+            if ia_dict:
+                d["IndividualAttributes"] = ia_dict
+
+        if self.individual_properties:
+            ip_dict = {"IndividualProperties": []}
+            for ip in self.individual_properties:
+                ip_dict["IndividualProperties"].append(ip.to_dict())
+            d.update(ip_dict)
+
+        d.update(self.meta)
+        return d
